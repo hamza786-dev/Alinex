112a10dfc7681e79cbbb63a7a846d2a55a1e4837
Hamza Ali <hamzaali99d@gmail.com>
2026-09-15 16:50:40 +0500

    Remove TeamSection

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Connections from "./Connections/Connections";
-import TeamSection from "./TeamSection/TeamSection";
+// import TeamSection from "./TeamSection/TeamSection";
 import ScrollGallery from "./ScrollGallery/ScrollGallery";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Connections/>} />
-        <Route path="/team" element={<TeamSection/>} />
+        {/* <Route path="/team" element={<TeamSection/>} /> */}
         <Route path="/gallery" element={<ScrollGallery/>} />
       </Routes>
     </Router>
